3a4a794bf9aa901df3d8a806f2e0e0bddc795f6f
tharun8748 <2200030888cser@gmail.com>
2026-08-11 20:10:31 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const neo4j = require("neo4j-driver");
 
 const driver = neo4j.driver(
-  process.env.COGNODB_URI,
+  process.env.NEO4J_URI,
   neo4j.auth.basic(
-    process.env.COGNODB_USERNAME,
-    process.env.COGNODB_PASSWORD
+    process.env.NEO4J_USERNAME,
+    process.env.NEO4J_PASSWORD
   )
 );
 
