f6bab70f0203b1a2cb38779ab1a5e5db3f051952
tharun8748 <2200030888cser@gmail.com>
2026-08-11 21:12:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://lostlink-nnjd.onrender.com/api";
 
 function Icon({ name, size = 20 }) {
   const common = {
